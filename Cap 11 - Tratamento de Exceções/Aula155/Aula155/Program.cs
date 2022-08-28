7cc33232db593805aaf4d8dc8caf6363f7e97239
Rafael Rodrigues <rafaelkunzler@gmail.com>
2022-08-28 09:42:26 -0300

    Exercicio tratamento de exceções

---
+++ Cap 11 - Tratamento de Exceções/Aula155/Aula155/Program.cs
@@ -0,0 +1,45 @@
+﻿using Aula155.Entities;
+using Aula155.Entities.Exceptions;
+
+namespace Aula155
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double limit = double.Parse(Console.ReadLine());
+
+                Account account = new Account(number, holder, balance, limit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine());
+            try
+            {
+                account.Withdraw(amount);
+                Console.WriteLine("New balance : " + account.Balance.ToString("F2"));
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error in format: " + e.Message);
+            }
+            catch (DomainExceptions e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
