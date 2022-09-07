6351b575f32ae3b989867f15f016de086421beac
Rafael Rodrigues <rafaelkunzler@gmail.com>
2022-09-07 09:27:01 -0300

    Exercicio de criacao de pastas e arquivos

---
+++ TrabalhandoComArquivos/TrabalhandoComArquivos/Program.cs
@@ -0,0 +1,44 @@
+﻿using Entities;
+using System.Globalization;
+
+namespace TrabalhandoComArquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\rafae\OneDrive\Área de Trabalho\Programação\C#\Exercicios C#\TrabalhandoComArquivos\source.csv";
+           
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourcePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach(string line in lines)
+                    {
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        sw.WriteLine(prod.Name + "," + prod.totalPrice().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
