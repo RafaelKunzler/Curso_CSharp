1d211e20505360b76d253d1c0d3a643425cad844
Rafael Rodrigues <rafaelkunzler@gmail.com>
2022-09-08 07:57:53 -0300

    Exercicio Interface

---
+++ Cap 14 - Interface/ContractPaymentFee/ContractPaymentFee/Interface/ContractService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractPaymentFee.Interface
+{
+    internal class ContractService
+    {
+    }
+}
---
+++ Cap 14 - Interface/ContractPaymentFee/ContractPaymentFee/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ContractPaymentFee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
